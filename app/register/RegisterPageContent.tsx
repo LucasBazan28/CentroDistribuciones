6f1d36736b1c384a89f23059799952eda8bed682
Lucas Bazan <Lucasbazan28@gmail.com>
2026-05-14 15:59:06 -0300

    Auto logout when accessing login/register with active session

--- app/register/RegisterPageContent.tsx
+++ app/register/RegisterPageContent.tsx
@@ -1,6 +1,11 @@
+"use client";
+
+import { useProtectAuthPages } from "@/lib/hooks/useProtectAuthPages";
 import RegisterForm from "@/app/components/RegisterForm";
 
 export default function RegisterPage() {
+  useProtectAuthPages();
+
   return (
     <main>
       <RegisterForm />
